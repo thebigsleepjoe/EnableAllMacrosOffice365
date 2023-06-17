1d4f66ed95c0f1af34a24e9bc5ff39085da16f55
thebigsleepjoe <warmharddeath1@gmail.com>
2023-06-16 18:03:18 -0700

    initial commit

---
+++ enablemacros.ps1
@@ -0,0 +1,22 @@
+$ENABLE_ALL = 1
+# $ASK_FIRST = 2 # Included for completeness, but not used here
+
+function Set-VBAWarningsFor($appPath, $val) {
+    Write-Output "Writing VBAWarnings to " $appPath
+
+    # Verify the path exists:
+    if (!(Test-Path $appPath)) {
+        Write-Output "Path does not exist; creating path " $appPath
+        New-Item -Path $appPath -Force | Out-Null
+        return
+    }
+
+    # Set the value
+    Set-ItemProperty -Path $path -Name VBAWarnings -Value $val
+}
+
+$excel16 = "HKCU:\Software\Microsoft\Office\16.0\Excel\Security"
+$word16 = "HKCU:\Software\Microsoft\Office\16.0\Word\Security"
+
+Set-VBAWarningsFor $excel16 $ENABLE_ALL
+Set-VBAWarningsFor $word16 $ENABLE_ALL
